8defc2f687998368cde0fba87783a8d3691399cd
Yevhen Danilov <danilov.evgen@gmail.com>
2021-09-07 14:25:26 +0300

    start files

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,20 @@
+const bodyTarg = document.querySelector("body");
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+startBtn.addEventListener("click", changeBgColor);
+
+function changeBgColor(event) {
+
+    timerId = setInterval(() => {
+        let color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+        bodyTarg.style.backgroundColor = color;
+    }, 1000);
+
+    startBtn.disabled = true;
+}
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+});
